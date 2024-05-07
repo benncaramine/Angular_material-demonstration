14d68cdaac19d3f616fac7197a2863329e2a6db6
benncaramine <aminebenncar0@gmail.com>
2024-05-07 15:53:01 +0100

    vf

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,14 +11,13 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 })
 export class RegisterComponent implements OnInit{
 
-
   public loginFormGroup! : FormGroup;
   hide = true;
   constructor(private fb : FormBuilder,
               private authService : AuthenticationService,
               private router : Router,
-              private snackBar: MatSnackBar) {
-  }
+              private snackBar: MatSnackBar) {}
+
   ngOnInit() {
     this.loginFormGroup = this.fb.group({
       username : this.fb.control(''),
@@ -27,14 +26,6 @@ export class RegisterComponent implements OnInit{
   }
   errorMessage: string | undefined;
   login() {
-    this.snackBar.open('Access Pending Administrator Approval !', 'Dismiss',  {
-      duration: 5000, // Duration in milliseconds
-      horizontalPosition: "center",
-      verticalPosition: "bottom"
-    });
-  }
-
-  register() {
     this.snackBar.open('Access Pending Administrator Approval !', 'Dismiss',  {
       duration: 5000, // Duration in milliseconds
       horizontalPosition: "center",
